c9a88907681b76ca8ba2f358f4fe0c8aeffc3d80
20German03 <117192011+20German03@users.noreply.github.com>
2026-05-04 18:28:54 +0200

    Update 05_entrenamiento_yolo.py
    Gestión de rutas actualizada

--- src/05_entrenamiento_yolo.py
+++ src/05_entrenamiento_yolo.py
@@ -1,15 +1,23 @@
 from ultralytics import YOLO
 import torch
+import tkinter as tk
+from tkinter import filedialog
 
 torch.backends.cudnn.benchmark = True
 
 if __name__ == '__main__':
+    root = tk.Tk()
+    root.withdraw()
+
+    RUTA_YAML = filedialog.askopenfilename(title="Selecciona el archivo data.yaml de tu dataset", filetypes=[("Archivos YAML", "*.yaml"), ("Todos los archivos", "*.*")])
+    CARPETA_PROYECTO = filedialog.askdirectory(title="Selecciona la carpeta donde se guardarán los resultados del proyecto (ej. resultados/fase2)")
+
     model = YOLO('yolov8s.pt') 
 
     print("<--- Iniciando el entrenamiento --->")
     #entrenamiento
     resultados = model.train(
-        data=r'C:\Users\GERMAN\Desktop\universidad\5_quinto\Trabajo_de_Fin_de_Grado\entrenamiento\dataset_tfg\data.yaml', 
+        data=RUTA_YAML, 
         epochs=300,       
         batch=8,          
         imgsz=640, 
@@ -20,7 +28,7 @@ if __name__ == '__main__':
         hsv_s=0.5,        
         hsv_v=0.5,
         
-        project=r'C:\Users\GERMAN\Desktop\universidad\5_quinto\Trabajo_de_Fin_de_Grado\resultados\fase2',
+        project=CARPETA_PROYECTO,
         name='entrenamiento_yolo' #nombre de la carpeta que se va a crear dentro de fase2
     )
     print("Fin del entrenamiento")
