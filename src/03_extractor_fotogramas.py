eaf407f5c4813f5f62df262630a1c291f890a692
20German03 <117192011+20German03@users.noreply.github.com>
2026-05-04 19:02:35 +0200

    Update 03_extractor_fotogramas.py
    gestion de carpetas actualizada

--- src/03_extractor_fotogramas.py
+++ src/03_extractor_fotogramas.py
@@ -4,9 +4,7 @@ import math
 from tkinter import filedialog
 from tkinter import Tk
 
-CARPETA_RESULTADOS = r'C:\Users\GERMAN\Desktop\universidad\5_quinto\Trabajo_de_Fin_de_Grado\resultados\fase2'
-
-#creacion de ventana
+# creacion de ventana
 root = Tk()
 root.withdraw() # ocultamos la ventana principal de fondo de tkinter
 root.attributes('-topmost', True)
@@ -23,17 +21,24 @@ if not ruta_video:
     print("No se ha seleccionado ningun video. Cancelando operación...")
     exit()
 
-#se crea una carpeta para guardar los frames
+# le pedimos al usuario que seleccione la carpeta donde guardar los resultados
+CARPETA_RESULTADOS = filedialog.askdirectory(title="Selecciona la carpeta base donde guardar los fotogramas")
+
+if not CARPETA_RESULTADOS:
+    print("No se ha seleccionado ninguna carpeta. Cancelando operación...")
+    exit()
+
+# se crea una carpeta para guardar los frames
 nombre_video_limpio = os.path.splitext(os.path.basename(ruta_video))[0]
 carpeta_destino = os.path.join(CARPETA_RESULTADOS, f"{nombre_video_limpio}_fotogramas")
 
-#se crea si no existe
+# se crea si no existe
 if not os.path.exists(carpeta_destino):
     os.makedirs(carpeta_destino)
 
 print(f"<--- Video seleccionado: {ruta_video} --->")
 
-#configuracion del video igual que en el anterior script
+# configuracion del video igual que en el anterior script
 cap = cv2.VideoCapture(ruta_video)
 fps = cap.get(cv2.CAP_PROP_FPS)
 fps = math.ceil(fps) if fps > 0 else 30 
@@ -41,7 +46,7 @@ fps = math.ceil(fps) if fps > 0 else 30
 total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
 duracion_total = total_frames / fps
 
-#si el video dura mas de 2 minutos, se recortan el primero y el ultimo
+# si el video dura mas de 2 minutos, se recortan el primero y el ultimo
 if duracion_total > 120:
     segundo_inicio = 60
     segundo_fin = int(duracion_total) - 60
@@ -50,20 +55,20 @@ else:
     segundo_fin = int(duracion_total)
 
 segundo_actual = segundo_inicio 
-#se define cada cuantos segundos se extrae un frame
+# se define cada cuantos segundos se extrae un frame
 salto_segundos = 5 
 
 print(f"Video cargado. FPS: {fps}. Empezando extraccion del segundo {segundo_inicio} al {segundo_fin}...")
 
-#bucle para procesar el video y extraer fotos
+# bucle para procesar el video y extraer fotos
 while cap.isOpened():
-    #control de finalizacion
+    # control de finalizacion
     if segundo_actual > segundo_fin:
         break
 
-    #salta al frame y con read se decodifica a matriz de pixeles
+    # salta al frame y con read se decodifica a matriz de pixeles
     frame_id = segundo_actual * fps
-    #esta parte hace un salto sin tener que pasar por todos los frames que no nos interesan
+    # esta parte hace un salto sin tener que pasar por todos los frames que no nos interesan
     cap.set(cv2.CAP_PROP_POS_FRAMES, frame_id)
     
     ret, frame = cap.read()
